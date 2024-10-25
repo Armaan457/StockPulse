f20197330587175e31d0a98f777bb4c28015fffa
lakshaysawhney <lsawhney07@gmail.com>
2024-10-25 23:14:42 +0530

    Adding Portfolio Backend Functionality

--- server/Stock_pulse/NewsSection/views.py
+++ server/Stock_pulse/NewsSection/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from .models import Portfolio
+from .serializers import PortfolioSerializer
 
-# Create your views here.
+class PortfolioListCreateView(generics.ListCreateAPIView):
+    serializer_class = PortfolioSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Portfolio.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class PortfolioDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = PortfolioSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Portfolio.objects.filter(user=self.request.user)
