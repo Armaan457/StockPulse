7b8be65b6e05cad2b37e223d1fac1ca041bf9ca2
himanishpuri <himanishpuri2203@gmail.com>
2024-10-26 00:23:17 +0530

    news card

--- client/src/Pages/News.tsx
+++ client/src/Pages/News.tsx
@@ -1,53 +1,74 @@
-import { Component } from "../App.tsx";
-
-import {
-	Carousel,
-	CarouselContent,
-	CarouselItem,
-	CarouselNext,
-	CarouselPrevious,
-} from "@/components/ui/carousel";
-
-import Autoplay from "embla-carousel-autoplay";
-
-const arr: JSX.Element[] = [
-	<Component key="component-1" />,
-	<Component key="component-2" />,
-	<Component key="component-3" />,
-	<Component key="component-4" />,
-	<Component key="component-5" />,
-	<Component key="component-6" />,
-];
+import NewsCard from "@/Part/NewsCard.tsx";
+import { useSidebar } from "@/components/ui/sidebar";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
 
 const News = () => {
+	const { open } = useSidebar();
+	const { register, handleSubmit } = useForm();
+
+	const onSubmit = () => {};
+
 	return (
-		<Carousel
-			className=" mx-20"
-			opts={{
-				align: "center",
-				loop: true,
-			}}
-			plugins={[
-				Autoplay({
-					delay: 4000,
-					stopOnMouseEnter: true,
-					stopOnInteraction: false,
-				}),
-			]}
-		>
-			<CarouselPrevious />
-			<CarouselContent>
-				{arr.map((item: JSX.Element, index: number) => (
-					<CarouselItem
-						key={index + item.type.name}
-						className="max-w-lg flex-shrink-0"
-					>
-						{item}
-					</CarouselItem>
-				))}
-			</CarouselContent>
-			<CarouselNext />
-		</Carousel>
+		<>
+			<div
+				className={`grid grid-cols-2 place-items-center gap-y-5 mx-28 ${
+					open ? "gap-x-[20vw]" : "gap-x-[37vw]"
+				}`}
+			>
+				<h1 className="text-3xl font-semibold text-green-700">Winners</h1>
+				<h1 className="text-3xl font-semibold text-red-700">Losers</h1>
+				<div className="grid grid-cols-2 grid-rows-2 ">
+					<NewsCard
+						companyName="Apple"
+						stockTrend={23}
+					/>
+					<NewsCard
+						companyName="Starbucks"
+						stockTrend={12}
+					/>
+					<NewsCard
+						companyName="Meta"
+						stockTrend={76}
+					/>
+					<NewsCard
+						companyName="Google"
+						stockTrend={56}
+					/>
+				</div>
+				<div className="grid grid-cols-2 grid-rows-2">
+					<NewsCard
+						companyName="Berkshir Hathaway"
+						stockTrend={-23}
+					/>
+					<NewsCard
+						companyName="Lemon Brothers"
+						stockTrend={-100}
+					/>
+					<NewsCard
+						companyName="Nokia"
+						stockTrend={-12}
+					/>
+					<NewsCard
+						companyName="Jaguar"
+						stockTrend={-34}
+					/>
+				</div>
+			</div>
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className="flex min-w-96 flex-col justify-center mt-5 gap-y-4"
+			>
+				<Input
+					type="text"
+					placeholder="Search for a company"
+					className="w-1/3 mx-auto px-5 py-5"
+					{...register("companyName")}
+				/>
+				<Button className="mx-auto w-2/12 py-5">Search</Button>
+			</form>
+		</>
 	);
 };
 
